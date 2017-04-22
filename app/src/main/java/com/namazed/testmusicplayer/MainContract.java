4ca8ee4c3ae15f87bb2b55cfd831ee670524d066
Namazed <actek17@gmail.com>
2017-04-22 13:26:17 +0700

    Created basis of MVP

--- app/src/main/java/com/namazed/testmusicplayer/MainContract.java
+++ app/src/main/java/com/namazed/testmusicplayer/MainContract.java
@@ -7,10 +7,12 @@ import com.hannesdorfmann.mosby3.mvp.MvpView;
 public interface MainContract {
 
     interface Presenter extends MvpPresenter<MainContract.View> {
-
+        void loadListSongs();
     }
 
     interface View extends MvpView {
+        void showProgress();
 
+        void hideProgress();
     }
 }
--- app/src/main/java/com/namazed/testmusicplayer/MainPresenter.java
+++ app/src/main/java/com/namazed/testmusicplayer/MainPresenter.java
@@ -2,8 +2,34 @@ package com.namazed.testmusicplayer;
 
 import com.hannesdorfmann.mosby3.mvp.MvpBasePresenter;
 
+import io.reactivex.disposables.CompositeDisposable;
+
 
 public class MainPresenter
         extends MvpBasePresenter<MainContract.View> implements MainContract.Presenter {
 
+    private CompositeDisposable compositeDisposable;
+
+    @Override
+    public void attachView(MainContract.View view) {
+        super.attachView(view);
+        compositeDisposable = new CompositeDisposable();
+    }
+
+    @Override
+    public void loadListSongs() {
+        if (!isViewAttached()) {
+            return;
+        }
+
+    }
+
+    @Override
+    public void detachView(boolean retainInstance) {
+        super.detachView(retainInstance);
+        if (!compositeDisposable.isDisposed()) {
+            compositeDisposable.dispose();
+            compositeDisposable.clear();
+        }
+    }
 }
